a774e328df9e8c4fd56502495da8663ae8c584fb
Patrick Lau <patricklyc@protonmail.com>
2025-07-23 16:42:38 -0700

    add buildah shim

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -4,5 +4,6 @@
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/buildah
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
