5fdfdf29929ec9de449ded81ac20e232c91de570
Patrick Lau <patricklyc@protonmail.com>
2025-08-01 15:22:47 -0700

    revert for little improvement

--- scripts/fedora-build.sh
+++ scripts/fedora-build.sh
@@ -3,12 +3,14 @@
 # Symlink distrobox shims
 ./distrobox-shims.sh
 
-# echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf
+echo "max_parallel_downloads=20" | sudo tee -a /etc/dnf/dnf.conf
 
-# sudo dnf install -y \
-#   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
-#   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+sudo dnf install -y \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
-# sudo dnf upgrade --refresh -y ; sudo dnf -y clean all
+sudo dnf upgrade --refresh -y ; sudo dnf -y clean all
 
-grep -v '^#' ./fedora.packages | xargs sudo dnf install -y ; sudo dnf -y clean all
+grep -v '^#' ./fedora.packages | xargs sudo dnf install -y
+
+sudo dnf -y clean all
